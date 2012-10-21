40740607c083c540e567797b5c366da87ddab03a
Emile Victor <emilevictor@gmail.com>
2012-10-21 14:25:32 +1000

    Adding all files to the repo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WITqrcodegenerator::Application.config.secret_token = 'e1e881d8448c118737a97b4298c4994809a98d0c090e01b7ba0053c577bf8c9d18c15b28299804003b0455bf066fd30b6beeee278a44e21cd097fae52193f990'
